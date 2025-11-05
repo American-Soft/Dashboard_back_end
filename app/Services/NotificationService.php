d6064f6d71d4ecf680770a88bf1c77c62eb049d9
mostafa <mghareeb841@gmail.com>
2025-11-05 10:08:54 +0200

    final change

--- app/Services/NotificationService.php
+++ app/Services/NotificationService.php
@@ -20,12 +20,24 @@ class NotificationService implements NotificationServiceInterface{
 
     public function unreadNotification(Request $request){
         $notifications = $request->user()->unreadNotifications;
-        return ['data' => $notifications , 'message' => 'Notifications Retrieved Success' , 'status' => 200];
+        $final_notifications = [];
+        foreach ($notifications as $notification) {
+            if ($this->requestRepository->checkById($notification->data['request_id'])) {
+                $final_notifications[] = $notification;
+            }
+        }
+        return ['data' => $final_notifications , 'message' => 'Notifications Retrieved Success' , 'status' => 200];
     }
 
     public function readNotification(Request $request){
         $notifications = $request->user()->readNotifications;
-        return ['data' => $notifications , 'message' => 'Notifications Retrieved Success' , 'status' => 200];
+        $final_notifications = [];
+        foreach ($notifications as $notification) {
+            if ($this->requestRepository->checkById($notification->data['request_id'])) {
+                $final_notifications[] = $notification;
+            }
+        }
+        return ['data' => $final_notifications , 'message' => 'Notifications Retrieved Success' , 'status' => 200];
     }
 
     public function markAsRead(Request $request , $notificationId){
